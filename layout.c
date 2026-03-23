e9b3d2c7b2e6da8cabea5af53ce357839516911a
danikchodo <01205862@pw.edu.pl>
2026-03-23 13:36:41 +0100

    layout.c
    poprawienie bledow zwiazanych ustawieniem poczatkowej pozycji w
    funkcji spectral_layout

--- layout.c
+++ layout.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <stdlib.h>
-#include <math.h>
 #include "layout.h"
 
 void fruchterman_reingold(graph *g,int i){
@@ -16,14 +14,19 @@ g->nodes[i].y=(double)(rand()%300);
 
 
 void spectral_layout(graph *g){
-if(g==NULL||g->nodes==NULL)
+if(g==NULL||g->nodes==NULL||g->node_count<2)
   return;
 
+  double start.x=100.0;
+  double start.y=100.0;
+  doudle koniec.x=300.0;
+  double koniec.y=300.0;
 
+double step.x=(koniec.x-start.x)/(g->node_count-1);
+double step.y=(koniec.y-start.y)/(g->node_count-1);
 
-double step=2*3.1415/g->node_count;
 for(int i=0;i<g->node_count;i++){
-g->nodes[i].x=400+200*cos(i*step);
-g->nodes[i].y=300+200*sin(i*step);
+  g->nodes[i].x=start.x+(i*step.x);
+  g->nodes[i].y=start.y+(i*step.y);
 }
 }
