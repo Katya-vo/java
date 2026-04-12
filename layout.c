b5959f35debf6791cfff9b79bb3b2d152f9abbdf
danikchodo <01205862@pw.edu.pl>
2026-04-12 03:39:06 +0200

    repulsing
    zaipmlementowanie odpychania w funkcji fruchterman-reingold

--- layout.c
+++ layout.c
@@ -1,10 +1,27 @@
 #include <stdlib.h>
+#include <math.h>
 #include "layout.h"
 
-void fruchterman_reingold(graph *g,int i){
+
+double sila_repuls(double d,double k)
+{
+return(k*k)/d;
+}
+double sila_attract(double d,double k)
+{
+retutn(d*d)/k;
+{
+
+void fruchterman_reingold(graph *g,int iterations){
   if(g==NULL||g->nodes==NULL)
     return;
 
+int n=g->node_count;
+double width=400.0;
+double height=300.0;
+double area=width*height;
+double k=sqrt(area/n);
+double t=width/10.0;
 
 
 for(int i=0;i<g->node_count;i++){
@@ -12,6 +29,28 @@ g->nodes[i].x=(double)(rand()%400);
 g->nodes[i].y=(double)(rand()%300);
 }
 
+double *dx=calloc(n,sizeof(double));
+double *dy=calloc(n,sizeof(double));
+for(int i=0;i<iterations;i++){
+for(int i=0;i<n;i++){
+dx[i]=0;
+dy[i]=0;
+for(int j=0;j<n;j++){
+if(i!=j){
+double vx=g->nodes[i].x-  g->nodes[j].x;
+double vy=g->nodes[i].y-  g->nodes[j].y;
+double distance=sqrt(vx*vx+vy*vy);
+if(distance<0.01)
+  dist=0.01;
+
+
+double f=sila_repuls(distance,k);
+dx[i]+=(vx/distance)*f;
+dy[i]+=(vy/distance)*f;
+}
+}
+}
+  
 
 void spectral_layout(graph *g){
 if(g==NULL||g->nodes==NULL||g->node_count<2)
