489dff1610978ad3dcb4885043bd68d1535bafa1
danikchodo <01205862@pw.edu.pl>
2026-04-13 11:21:30 +0200

    Update main.c
    poprawka logiki komunikacji plikow

--- main.c
+++ main.c
@@ -42,35 +42,45 @@ int main(int argc, char *argv[]) {
     printf("plik wyjsciowy: %s\n", output_file);
     printf("format zapisu: %s\n", format);
 
-    graph *g = load_graph(input_file); 
-    if (g == NULL) {
-        fprintf(stderr, "Blad: nie udalo sie wczytac grafu\n");
+    FILE *fin=fopen(input_file,"r");
+    if (fin==NULL) {
+        fprintf(stderr, "blad:nie udalo sie otworzyc pliku %s\n",input_file);
         return 2;
     }
 
-    if (algo_type==1) {
-    printf("obliczanie algorytmem fruchterman-reingold\n");
-    } 
-    else if (algo_type==2) {
-    printf("obliczanie algorytmem spectral layout\n");
-    spectral_layout(g);
-    }
-    
-    if (save_graph(g, output_file, format) != 0) {
-        fprintf(stderr,"blad zapisywania do pliku %s\n", output_file);
-        free_graph(g);
-        return 3;
-    }
-    free_graph(g);
-  
-    if (algo_type==1) {
-        printf("algorytm:fruchterman-reingold\n");
-    } else if (algo_type == 2) {
-        printf("algorytm:spectral layout\n");
-    } else {
-        fprintf(stderr,"nieznany algorytm %d\n", algo_type);
-        return 4; 
+    int n, e;
+    if (fscanf(fin, "%d %d", &n, &e) != 2) {
+        fprintf(stderr,"blad:niewlasciwy format pliku \n");
+        fclose(fin);
+        return 2;
     }
 
+if (algo_type == 1) {
+        printf("obliczanie algorytmem fruchterman-reingold\n");
+        fruchterman_reingold(g,100);
+    else if (algo_type==2) {
+        printf("obliczanie algorytmem spectral layout\n");
+        spectral_layout(g);
+    } else {
+        fprintf(stderr,"nieznany algorytm %d\n", algo_type);
+        free_graph(g);
+        free(g);
+        return 4;
+    }
+
+    FILE *fout = fopen(output_file, "w");
+    if (fout == NULL) {
+        fprintf(stderr, "blad zapisyвания do pliku %s\n", output_file);
+        free_graph(g);
+        free(g);
+        return 3;
+    }
+
+    for (int i = 0; i < g->node_count; i++) {
+        fprintf(fout, "%f %f\n", g->nodes[i].x, g->nodes[i].y);
+    }
+        fclose(fout);
+        free_graph(g);
+        free(g);
     return 0;
 }
