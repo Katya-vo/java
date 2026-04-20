31fc206357908b090bd03819416d6a49e559f68c
danikchodo <01205862@pw.edu.pl>
2026-04-20 05:31:06 +0200

    Update main.c
    poprawienie funkcji zwiazanych z odczytem plikow i poprawa z odczytu binarnej formy

--- main.c
+++ main.c
@@ -5,26 +5,26 @@
 #include "graph.h"
 #include "layout.h"
 
-int main(int argc, char *argv[]) {
-    char *input_file = NULL;
-    char *output_file = NULL;
-    char *format = "txt"; 
-    int algo_type = 1;    
+int main(int argc,char *argv[]) {
+    char *input_file=NULL;
+    char *output_file=NULL;
+    char *format="txt"; 
+    int algo_type=1;    
 
     int opt;
-    while ((opt = getopt(argc, argv, "i:o:a:f:")) != -1) {
+    while ((opt=getopt(argc,argv,"i:o:a:f:"))!= -1) {
         switch (opt) {
             case 'i':
-                input_file = optarg;
+                input_file=optarg;
                 break;
             case 'o':
-                output_file = optarg;
+                output_file=optarg;
                 break;
             case 'a':
-                algo_type = atoi(optarg);
+                algo_type=atoi(optarg);
                 break;
             case 'f':
-                format = optarg; 
+                format=optarg; 
                 break;
             default:
                 fprintf(stderr,"uzycie: %s -i input -o output -a 1/2 -f txt/bin\n", argv[0]);
@@ -37,60 +37,40 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("konfiguracja uruchomienia:\n");
-    printf("plik wejsciowy: %s\n", input_file);
-    printf("plik wyjsciowy: %s\n", output_file);
-    printf("format zapisu: %s\n", format);
+    graph *g=malloc(sizeof(graph));
+        if (g==NULL) 
+        return 1;
 
-    FILE *fin=fopen(input_file,"r");
-    if (fin==NULL) {
-        fprintf(stderr, "blad:nie udalo sie otworzyc pliku %s\n",input_file);
+    if (readgraph(input_file,g) != 0) {
+        fprintf(stderr,"blad odczytu pliku %s\n",input_file);
+        free(g);
         return 2;
     }
 
-    int n, e;
-    if (fscanf(fin,"%d %d",&n,&e)!= 2) {
-        fprintf(stderr, "blad:niewlasciwy format pliku \n");
-        fclose(fin);
-        return 2;
-    }
-
-    graph *g = malloc(sizeof(graph)); 
-    init_graph(g, n, e); 
-    for (int i = 0; i < e; i++) {
-        fscanf(fin,"%d %d",&(g->edges[i].s), &(g->edges[i].t));
-    }
-    fclose(fin); 
-
-if (algo_type == 1) {
-        printf("obliczanie algorytmem fruchterman-reingold\n");
-        fruchterman_reingold(g,100);
-}
-    else if (algo_type==2) {
-        printf("obliczanie algorytmem spectral layout\n");
+    if (algo_type==1) {
+        fruchterman_reingold(g, 100);
+    } else if (algo_type==2) {
         spectral_layout(g);
-    } 
-    else
-    {
+    } else {
         fprintf(stderr,"nieznany algorytm %d\n", algo_type);
         free_graph(g);
         free(g);
         return 4;
     }
 
-    FILE *fout = fopen(output_file, "w");
-    if (fout==NULL) {
-        fprintf(stderr, "blad zapisyвания do pliku %s\n", output_file);
-        free_graph(g);
-        free(g);
-        return 3;
+    int save_status;
+    if (strcmp(format, "bin")==0) {
+        save_status=savebin(output_file, g);
+    } else {
+        save_status=savetxt(output_file, g);
     }
 
-    for (int i=0; i<g->node_count; i++) {
-        fprintf(fout,"%f %f\n",g->nodes[i].x,g->nodes[i].y);
+    if (save_status != 0) {
+        fprintf(stderr,"blad zapisu do pliku %s\n", output_file);
     }
-        fclose(fout);
-        free_graph(g);
-        free(g);
-    return 0;
+
+    free_graph(g);
+    free(g);
+
+    return save_status;
 }
