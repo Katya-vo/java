6d02750276e3bbc272f38e834c3709e0f13d30fb
danikchodo <01205862@pw.edu.pl>
2026-04-20 03:56:12 +0200

    Update layout.c
    poprawa funkcjii FR zgodnie z dodaniem wartości waga krawędzi

--- layout.c
+++ layout.c
@@ -14,85 +14,87 @@ double sila_attract(double d,double k)
 return(d*d)/k;
 }
 
-void fruchterman_reingold(graph *g,int iterations){
-  if(g==NULL||g->nodes==NULL)
+void fruchterman_reingold(graph*g, int iterations) {
+    if (g==NULL||g->nodes==NULL) 
     return;
 
-int n=g->node_count;
-double width=400.0;
-double height=300.0;
-double area=width*height;
-double k=sqrt(area/n);
-double t=width/10.0;
+    int n=g->node_count;
+    double width=400.0,
+    double height=300.0;
+    double area=width*height;
+    double k=sqrt(area/n);
+    double t=width/10.0;
+
+    for (int i=0;i<n;i++) {
+        g->nodes[i].x=(double)(rand()%400);
+        g->nodes[i].y=(double)(rand()%300);
+    }
+
+    double *dx=calloc(n,sizeof(double));
+    double *dy=calloc(n,sizeof(double));
+
+    for (int it = 0; it < iterations; it++) {
+        for (int i=0;i<n;i++) {
+            dx[i]=0;dy[i]=0;
+            for (int j=0;j<n;j++) {
+                if (i!=j) {
+                    double vx=g->nodes[i].x-g->nodes[j].x;
+                    double vy=g->nodes[i].y-g->nodes[j].y;
+                    double distance=sqrt(vx*vx+vy*vy);
+                    if (distance<0.01) 
+                    distance = 0.01;
+                    double f=sila_repuls(distance, k);
+                    dx[i]+=(vx/distance)*f;
+                    dy[i]+=(vy/distance)*f;
+                }
+            }
+        }
 
 
-for(int i=0;i<g->node_count;i++){
-g->nodes[i].x=(double)(rand()%400);
-g->nodes[i].y=(double)(rand()%300);
-}
+        for (int i=0;i<g->edge_count;i++) {
+            int u=g->edges[i].s;
+            int v=g->edges[i].t;
+            double weight=g->edges[i].weight; 
 
-double *dx=calloc(n,sizeof(double));
-double *dy=calloc(n,sizeof(double));
-for(int it=0;it<iterations;it++){
-for(int i=0;i<n;i++){
-dx[i]=0;
-dy[i]=0;
-for(int j=0;j<n;j++){
-if(i!=j){
-double vx=g->nodes[i].x-  g->nodes[j].x;
-double vy=g->nodes[i].y-  g->nodes[j].y;
-double distance=sqrt(vx*vx+vy*vy);
-if(distance<0.01)
-  distance=0.01;
+            double vx=g->nodes[u].x-g->nodes[v].x;
+            double vy=g->nodes[u].y-g->nodes[v].y;
+            double distance=sqrt(vx * vx + vy * vy);
+            if (distance<0.01) 
+            distance=0.01;
 
+   
+            double f = sila_attract(distance,k)*weight; 
+            
+            double shift_x = (vx/distance)*f;
+            double shift_y = (vy / distance)*f;
+            dx[u]-=shift_x; 
+            dy[u]-=shift_y;
+            dx[v]+=shift_x; 
+            dy[v]+=shift_y;
+        }
 
-double f=sila_repuls(distance,k);
-dx[i]+=(vx/distance)*f;
-dy[i]+=(vy/distance)*f;
-}
-}
-}
-
-
-for (int i=0;i<g->edge_count;i++){
-int u=g->edges[i].s;
-int v=g->edges[i].t;
-double vx=g->nodes[u].x - g->nodes[v].x;
-double vy=g->nodes[u].y - g->nodes[v].y;
-double distance=sqrt(vx*vx+vy*vy);
-if(distance<0.01)
-distance=0.01;
-
-double f=sila_attract(distance,k);
-double shift_x=(vx/distance)*f;
-double shift_y=(vy/distance)*f;
-dx[u]-=shift_x;
-dy[u]-=shift_y;
-dx[v]+=shift_x;
-dy[v]+=shift_y;
-}
-
-for(int i=0;i<n;i++){
-double disp_distance=sqrt(dx[i]*dx[i]+dy[i]*dy[i]);
-if(disp_distance>0){
-double lim_distance=(disp_distance<t)?disp_distance:t;
-g->nodes[i].x+=(dx[i]/disp_distance)*lim_distance;
-g->nodes[i].y+=(dy[i]/disp_distance)*lim_distance;
-}
-
-if(g->nodes[i].x<0)
-  g->nodes[i].x=0;
-if(g->nodes[i].x>width)
-  g->nodes[i].x=width;
-if(g->nodes[i].y<0)
-  g->nodes[i].y=0;
-if(g->nodes[i].y>height)
-  g->nodes[i].y=height;
-}
-t*=0.95;
-}
-free(dx);
-free(dy);
+     
+        for (int i = 0; i < n; i++) {
+            double disp_dist = sqrt(dx[i] * dx[i] + dy[i] * dy[i]);
+            if (disp_dist > 0) {
+                double lim = (disp_dist < t) ? disp_dist : t;
+                g->nodes[i].x += (dx[i] / disp_dist) * lim;
+                g->nodes[i].y += (dy[i] / disp_dist) * lim;
+            }
+            // Границы экрана
+            if (g->nodes[i].x<0) 
+            g->nodes[i].x=0;
+            if (g->nodes[i].x>width) 
+            g->nodes[i].x=width;
+            if (g->nodes[i].y<0)
+            g->nodes[i].y=0;
+            if (g->nodes[i].y>height)
+            g->nodes[i].y =height;
+        }
+        t*=0.95; 
+    }
+    free(dx); 
+    free(dy);
 }
 
 void moc_iteration(double *L,int n,double *vector,int iterations) {
@@ -114,9 +116,6 @@ vector[i]=next_v[i]/norm;
         free(next_v);
     }
 }
-
-
-
 
 
 void spectral_layout(graph *g){
