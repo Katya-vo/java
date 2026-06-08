92be726c3905d076555e7bd47115e6a71887ccc5
jagnaszapiel <01206827@pw.edu.pl>
2026-06-08 09:17:08 +0200

    Update and rename include_c/layout.h to graph-visualization/include/layout.h

---
+++ graph-visualization/include/layout.h
@@ -0,0 +1,7 @@
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+void circle_layout(double *x, double *y, int n);
+void random_layout(double *x, double *y, int n);
+
+#endif
--- include_c/layout.h
+++
@@ -1,6 +0,0 @@
-#include "graph.h"
-#ifndef LAYOUT_H
-#define LAYOUT_H
-void fruchterman_reingold(graph *g,int i);
-void spectral_layout(graph *g);
-#endif
