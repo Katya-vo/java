00f8f1af7b0b5f6171cf471886d7a34e2e017321
jagnaszapiel <01206827@pw.edu.pl>
2026-06-08 08:59:20 +0200

    Update GraphData.java

--- src/main/java_visualiser/model/GraphData.java
+++ src/main/java_visualiser/model/GraphData.java
@@ -4,11 +4,25 @@ public class GraphData {
     public static class Node {
         public int id;
         public double x, y;
-        public Node(int id, double x, double y) { this.id = id; this.x = x; this.y = y; }
+
+        public Node(int id, double x, double y) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+        }
     }
 
     public static class Edge {
-        public int sourceId, targetId;
-        public Edge(int s, int t) { this.sourceId = s; this.targetId = t; }
+        public String label;
+        public int sourceId;
+        public int targetId;
+        public double weight;
+
+        public Edge(String label, int s, int t, double w) {
+            this.label = label;
+            this.sourceId = s;
+            this.targetId = t;
+            this.weight = w;
+        }
     }
 }
